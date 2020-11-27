64c3fc55f67564bf354bb8980b723dcf5b37c2ca
lijw <113714806@qq.com>
2020-11-27 23:50:38 +0800

    在公共模块用Cors配置类解决跨域问题

---
+++ server/src/main/java/com/vi/server/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.vi.server.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    private static final Logger log = LoggerFactory.getLogger(CorsConfig.class);
+    /**
+     * 解决前后端分离跨域问题
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        log.info("====解决跨域问题====");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders(CorsConfiguration.ALL)
+                .allowedMethods(CorsConfiguration.ALL)
+                .allowCredentials(true)
+                .maxAge(3600);  // 1小时内不需要再预检
+    }
+}
