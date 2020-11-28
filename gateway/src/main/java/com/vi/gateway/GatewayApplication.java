8bb02f4d3ccbfbad8757a939c50757f3b2d9c288
lijw <113714806@qq.com>
2020-11-28 11:57:28 +0800

    在网关配置跨域，去掉server模块的跨域配置,修改网关路由转发的方式，用服务名替换硬编码

--- gateway/src/main/java/com/vi/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/vi/gateway/GatewayApplication.java
@@ -3,7 +3,12 @@ package com.vi.gateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @SpringBootApplication
 @Slf4j
@@ -13,6 +18,23 @@ public class GatewayApplication {
         ConfigurableEnvironment environment = SpringApplication.run(GatewayApplication.class, args).getEnvironment();
         log.info("gateway启动成功!");
         log.info("gateway地址:http://127.0.0.1:{}",environment.getProperty("server.port"));
+    }
+
+    /**
+     * 配置跨域
+     * @return
+     */
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(Boolean.TRUE);
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**",config);
+        return new CorsWebFilter(source);
     }
 
 }
