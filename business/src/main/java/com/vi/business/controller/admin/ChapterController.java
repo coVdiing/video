f2826339ae87030d04f605caa45883b9513d0eb0
lijw <113714806@qq.com>
2020-12-23 21:58:33 +0800

    大章列表加上分页，前端交互传入分页参数

--- business/src/main/java/com/vi/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/vi/business/controller/admin/ChapterController.java
@@ -2,10 +2,9 @@ package com.vi.business.controller.admin;
 
 import com.vi.server.domain.Chapter;
 import com.vi.server.dto.ChapterDto;
+import com.vi.server.dto.PageDto;
 import com.vi.server.service.ChapterService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -16,8 +15,9 @@ public class ChapterController {
     @Resource
     private ChapterService chapterService;
 
-    @GetMapping("/list")
-    public List<ChapterDto> list() {
-        return chapterService.list();
+    @RequestMapping("/list")
+    public PageDto list(@RequestBody PageDto pageDto) {
+         chapterService.list(pageDto);
+         return pageDto;
     }
 }
