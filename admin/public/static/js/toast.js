281306be262b66c81995d0330f66e70e36a70245
lijw <113714806@qq.com>
2021-04-24 14:54:31 +0800

    使用sweetalert封装消息提示框，在新增，删除，编辑时进行提示

---
+++ admin/public/static/js/toast.js
@@ -0,0 +1,38 @@
+import Swal from "sweetalert2";
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-middle',
+    showConfirmButton: false,
+    timer: 1000,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+});
+
+
+function alertSuccess(message) {
+    Toast.fire({
+        icon: 'success',
+        title: message
+    })
+}
+
+function alertError(message) {
+    Toast.fire({
+        icon: 'error',
+        title: message
+    })
+}
+
+function alertWarn(message) {
+    Toast.fire({
+        icon: 'warning',
+        title: message
+    })
+}
+
+
+export {alertSuccess,alertError,alertWarn}
