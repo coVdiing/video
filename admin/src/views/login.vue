24a9fff2dbac22b590fc95094807a72b97554e10
lijw <113714806@qq.com>
2021-06-05 14:39:57 +0800

    去掉记住我按钮

--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -40,10 +40,10 @@
                                                 </label>
                                                 <div class="space"></div>
                                                 <div class="clearfix">
-                                                    <label class="inline">
-                                                        <input type="checkbox" class="ace"/>
-                                                        <span class="lbl">记住我</span>
-                                                    </label>
+<!--                                                    <label class="inline">-->
+<!--                                                        <input type="checkbox" class="ace"/>-->
+<!--                                                        <span class="lbl">记住我</span>-->
+<!--                                                    </label>-->
                                                     <button type="button"
                                                             class="width-35 pull-right btn btn-sm btn-primary"
                                                             @click="login()">
